df75273724bd069612b3f5b7dc10d1bf345f2537
shiyeli <422013052@qq.com>
2017-04-29 18:38:00 +0800

    登录登出完成

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ URL: http://0.0.0.0:8101/main/ + ...
 
 ### 注册登录
 
-#### 注册signup(post)
+#### 注册:signup(post)
 >phone(11) email password(>6) password2
+
+#### 登录:signin(post)
+>phone(11) password
+
+#### 登出:signout(get)
+
+
+
 
--- huxianghui/main/views.py
+++ huxianghui/main/views.py
@@ -1,4 +1,5 @@
 #-*- coding: utf-8 -*-
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -6,13 +7,14 @@ from django.shortcuts import render
 
 
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
+from django.views.decorators.http import require_POST, require_GET
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.http import HttpResponse, JsonResponse, HttpResponseNotFound, HttpResponseBadRequest
 from models import *
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 
 #个人中心
@@ -39,9 +41,34 @@ def signup(request):
         user=User.objects.create_user(username=phone,password=password,email=email)
         profile=Profile(user=user,phone=phone,email=email)
         profile.save()
+        print 'success!'
     except:
         return HttpResponseBadRequest('注册失败')
     return HttpResponse('注册成功')
 
 
+@require_POST
+@csrf_exempt
+def signin(request):
+    params=request.POST
+    try:
+        phone=params['phone']
+        password=params['password']
+    except:
+        HttpResponseBadRequest('参数不正确')
 
+    user=authenticate(username=phone,password=password)
+    if user is not None:
+        if user.is_active:
+            login(request,user)
+            return HttpResponse('登录成功')
+        else:
+            return HttpResponseBadRequest('账号未激活')
+    return HttpResponseBadRequest('账号或密码不对')
+
+
+
+@require_GET
+def signout(request):
+    logout(request)
+    return HttpResponse('登出成功')
