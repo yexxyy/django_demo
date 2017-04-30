531616004aa46bf9fd8cfee1629905b11064a998
shiyeli <422013052@qq.com>
2017-04-30 23:12:43 +0800

    调整model代码位置

--- huxianghui/main/models.py
+++ huxianghui/main/models.py
@@ -55,102 +55,6 @@ RECOMMADN_IDS=(
 )
 
 
-
-
-###################个人中心start##################
-
-# @python_2_unicode_compatible
-# class LikeRegion(models.Model):
-#     class Meta:
-#         verbose_name = '意向区域'
-#         verbose_name_plural = '意向区域'
-#     region=models.CharField(max_length=10,verbose_name='区域')
-#
-#     def __str__(self):
-#         return self.region
-#
-#
-#
-
-
-
-@python_2_unicode_compatible
-class Profile(models.Model):
-    class Meta:
-        verbose_name='用户中心'
-        verbose_name_plural = '用户中心'
-    user=models.OneToOneField(User,on_delete=models.CASCADE)
-    name=models.CharField(max_length=20,verbose_name='姓名',null=True,blank=True,default='')
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, verbose_name='性别', blank=True,null=True)
-    age=models.IntegerField(verbose_name='年龄',blank=True,null=True)
-    address = models.CharField(max_length=50, verbose_name='住址',blank=True,default='')
-    regions=models.CharField(max_length=10,choices=LOCATIONS,verbose_name='意向区域',blank=True)
-    styles=models.CharField(max_length=10,choices=BUILDING_TYPE,verbose_name='意向类型',blank=True)
-
-    def __str__(self):
-        return self.phone
-
-
-
-
-
-
-
-
-
-################### 个人中心结束 ##################
-
-
-################### Index start ##################
-@python_2_unicode_compatible
-class News(models.Model):
-    class Meta:
-        verbose_name='新闻链接'
-        verbose_name_plural='新闻链接'
-
-    news_name=models.CharField(max_length=20,verbose_name='名称',default='')
-    news_url = models.CharField(max_length=200, verbose_name='链接', default='')
-
-    def __str__(self):
-        return self.news_name
-
-    def to_json(self):
-        this={
-            'id':self.pk,
-            'news_url':self.news_url,
-            'news_name':self.news_name,
-        }
-        return this
-
-
-@python_2_unicode_compatible
-class Banner(models.Model):
-    class Meta:
-        verbose_name = '顶部广告'
-        verbose_name_plural = '顶部广告'
-
-    cover = models.ImageField(upload_to=pic_upload_path, verbose_name='封面图',default='')
-    detail_url = models.CharField(max_length=100, verbose_name='详情链接',default='')
-    recommend_id = models.IntegerField(verbose_name='推荐指数(填写：1、2、3...)')
-
-    def get_cover_url(self):
-        return self.cover.url if self.cover else ''
-
-    def __str__(self):
-        return self.get_cover_url()
-
-    def to_json(self):
-        this={
-            'id':self.pk,
-            'cover':self.get_cover_url(),
-            'detail_url':self.detail_url,
-            'recommend_id':self.recommend_id,
-        }
-        return this
-
-################### Index end ##################
-
-
 ################### building start ##################
 
 @python_2_unicode_compatible
@@ -203,24 +107,82 @@ class Building(models.Model):
         return this
 
 
+################### building end ##################
+
+###################个人中心start##################
+
+
 @python_2_unicode_compatible
-class UserLike(models.Model):
+class Profile(models.Model):
     class Meta:
-        verbose_name='用户收藏'
-        verbose_name_plural='用户收藏'
+        verbose_name='用户中心'
+        verbose_name_plural = '用户中心'
     user=models.OneToOneField(User,on_delete=models.CASCADE)
-    liked=models.ManyToManyField(Building,verbose_name='已收藏',blank=True)
+    name=models.CharField(max_length=20,verbose_name='姓名',null=True,blank=True,default='')
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, verbose_name='性别', blank=True,null=True)
+    age=models.IntegerField(verbose_name='年龄',blank=True,null=True)
+    address = models.CharField(max_length=50, verbose_name='住址',blank=True,default='')
+    regions=models.CharField(max_length=10,choices=LOCATIONS,verbose_name='意向区域',blank=True)
+    styles=models.CharField(max_length=10,choices=BUILDING_TYPE,verbose_name='意向类型',blank=True)
 
     def __str__(self):
-        return self.user.name
+        return self.phone
+
+################### 个人中心结束 ##################
 
 
 
+################### Index start ##################
+@python_2_unicode_compatible
+class News(models.Model):
+    class Meta:
+        verbose_name='新闻链接'
+        verbose_name_plural='新闻链接'
+
+    news_name=models.CharField(max_length=20,verbose_name='名称',default='')
+    news_url = models.CharField(max_length=200, verbose_name='链接', default='')
+
+    def __str__(self):
+        return self.news_name
+
+    def to_json(self):
+        this={
+            'id':self.pk,
+            'news_url':self.news_url,
+            'news_name':self.news_name,
+        }
+        return this
+
+
+@python_2_unicode_compatible
+class Banner(models.Model):
+    class Meta:
+        verbose_name = '顶部广告'
+        verbose_name_plural = '顶部广告'
+
+    cover = models.ImageField(upload_to=pic_upload_path, verbose_name='封面图',default='')
+    detail_url = models.CharField(max_length=100, verbose_name='详情链接',default='')
+    recommend_id = models.IntegerField(verbose_name='推荐指数(填写：1、2、3...)')
+
+    def get_cover_url(self):
+        return self.cover.url if self.cover else ''
+
+    def __str__(self):
+        return self.get_cover_url()
+
+    def to_json(self):
+        this={
+            'id':self.pk,
+            'cover':self.get_cover_url(),
+            'detail_url':self.detail_url,
+            'recommend_id':self.recommend_id,
+        }
+        return this
+
+################### Index end ##################
 
 
 
-
-################### building end ##################
 
 
 ################### 活动 ##################
@@ -297,13 +259,6 @@ class Activity(models.Model):
             'cover':self.get_cover_url(),
         }
         return this
-
-
-
-
-
-
-
 
 
 ################### ／活动 ##################
