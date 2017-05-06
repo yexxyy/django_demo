4bb106f0dee76d2ba887504380a34ae5fe5b0b5d
shiyeli <422013052@qq.com>
2017-05-07 00:07:34 +0800

    改中文

--- huxianghui/huxianghui/settings/settings.py
+++ huxianghui/huxianghui/settings/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'huxianghui.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -95,8 +95,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-# LANGUAGE_CODE = 'zh_hans'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh_hans'
 
 TIME_ZONE = 'Asia/Shanghai'
 
