cd2128ecee078ff91690c30a9edfa88f809a7bc3
shiyeli <422013052@qq.com>
2017-05-07 13:08:40 +0800

    添加html url

--- huxianghui/huxianghui/views.py
+++ huxianghui/huxianghui/views.py
@@ -14,5 +14,47 @@ def get_asstes(request,file_path):
     return response
 
 
-def get_index(request):
+def index(request):
     return render(request, 'build/moc/index.html')
+
+def list(request):
+    return render(request, 'build/moc/list.html')
+
+def form(request):
+    return render(request, 'build/moc/form.html')
+
+def person(request):
+    return render(request, 'build/moc/person.html')
+
+def reg_change(request):
+    return render(request, 'build/moc/reg_change.html')
+
+def reg_forget(request):
+    return render(request, 'build/moc/reg_forget.html')
+
+def reg_info(request):
+    return render(request, 'build/moc/reg_info.html')
+
+def reg_like(request):
+    return render(request, 'build/moc/reg_like.html')
+
+def reg_login(request):
+    return render(request, 'build/moc/reg_login.html')
+
+def reg_register(request):
+    return render(request, 'build/moc/reg_register.html')
+
+def search(request):
+    return render(request, 'build/moc/search.html')
+
+def select(request):
+    return render(request, 'build/moc/select.html')
+
+def select_input(request):
+    return render(request, 'build/moc/select_input.html')
+
+def select_score(request):
+    return render(request, 'build/moc/select_score.html')
+
+def test_detail(request):
+    return render(request, 'build/moc/test-detail.html')
