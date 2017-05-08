ba95f1c2e2d34872f2ca7555e7a4bc3669b54891
宋竹梅 <zhumei.song@monstar-lab-cn.com>
2017-05-09 07:01:42 +0800

    5.8 index

--- huxianghui/main/static/javascript/app.js
+++ huxianghui/main/static/javascript/app.js
@@ -1189,7 +1189,7 @@
             var $defer = $.Deferred();
             var options = {
                 type: 'get',
-                url: host.path_join('static/data/select.json')
+                url: '../data/select.json'
             };
             Util.local_ajax(options).done(function (result) {
                 $defer.resolve(result);
