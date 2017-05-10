86f6c68992d8f4d6ed85315fdbba0eeccdc858b5
宋竹梅 <zhumei.song@monstar-lab-cn.com>
2017-05-10 14:25:07 +0800

    5.8 index

--- huxianghui/main/static/javascript/app.js
+++ huxianghui/main/static/javascript/app.js
@@ -1642,31 +1642,53 @@
         };
     })();
 
-    Page.like = (function () {
-        var init = function () {
-            mui.init();
-            mui.ready(function () {
-                Api.like_list.fetch()
+Page.like = (function () {
+    var init = function () {
+        mui.init();
+        mui.ready(function () {
+            Api.like_list.fetch()
+                .done(function (_data) {
+                    render(_data);
+                    bind();
+                })
+                .fail(function (err_msg, error) {
+                    console.log(err_msg);
+                });
+        });
+
+        var render = function (_data) {
+            console.log(_data);
+            if (_data.list.length > 0) {
+                var template = Handlebars.compile($('#template_like_cell').html());
+                $('#like_con').html(template(_data.list));
+            } else {
+                var div ='<div>暂无收藏</div>';
+                $('.empty-con').html(div);
+            }
+        };
+
+        var delete_cell = function () {
+            $('.delete-btn').on('tap', function () {
+                var id = $(this).data('id');
+                Api.like_set.fetch(id)
                     .done(function (_data) {
-                        render(_data);
-                        bind();
+                        mui.toast('删除成功');
+                        window.location.reload();
                     })
                     .fail(function (err_msg, error) {
                         console.log(err_msg);
                     });
             });
-            var render = function (_data) {
-                console.log(_data)
-                var template;
-            };
+        };
 
-            var bind = function () {
-                Util.go_to_detail($('.like-cell'))
-            }
-        };
-        return {
-            init: init
-        };
+        var bind = function () {
+            Util.go_to_detail($('.like-cell'));
+            delete_cell()
+        }
+    };
+    return {
+        init: init
+    };
 
     })();
 
