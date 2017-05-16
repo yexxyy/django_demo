66dfbcb5823eb7b20d68ccf57f445fc370e73e43
宋竹梅 <zhumei.song@monstar-lab-cn.com>
2017-05-16 11:02:25 +0800

    5.16

--- huxianghui/main/static/javascript/app.js
+++ huxianghui/main/static/javascript/app.js
@@ -347,30 +347,30 @@ Util.is_pc = function (pathname) {
         return $defer.promise();
     };
 
-    Util.active = function (pathname) {
-        if (pathname == Route.top) {
-            $('.img-index').addClass('item-active')
-        }
-        else if (pathname == Route.activity) {
-            $('.img-activities').addClass('item-active')
-        }
-        else if (pathname == Route.select) {
-            $('.img-select').addClass('item-active')
-            Util.call_login($('.nav-select'), 'select');
-        }
-        else if (pathname == Route.person) {
-            $('.img-person').addClass('item-active')
-            Util.call_login($('.nav-person'), 'person');
-        }
+Util.active = function (pathname) {
+    if (pathname == Route.top) {
+        $('.img-index').addClass('item-active')
+    }
+    else if (pathname == Route.activity) {
+        $('.img-activities').addClass('item-active')
+    }
+    else if (pathname == Route.select||pathname == Route.input||pathname == Route.score) {
+        $('.img-select').addClass('item-active')
+        Util.call_login($('.nav-select'), 'select');
+    }
+    else if (pathname == Route.person||pathname == Route.like||pathname == Route.info||pathname == Route.change) {
+        $('.img-person').addClass('item-active')
+        Util.call_login($('.nav-person'), 'person');
+    }
 
-        $('.nav-select').on('tap', function () {
-            Util.call_login($(this), 'select');
-        });
+    $('.nav-select').on('tap', function () {
+        Util.call_login($(this), 'select');
+    });
 
-        $('.nav-person').on('tap', function () {
-            Util.call_login($(this), 'person');
-        });
-    };
+    $('.nav-person').on('tap', function () {
+        Util.call_login($(this), 'person');
+    });
+};
 
     /**
      * 获取当前状态
